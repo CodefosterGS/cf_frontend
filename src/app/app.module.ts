9c38e175b4992336d6730a0d648e1abb0acf786d
Ashish Kumar Patel <ashish_3805@hotmail.com>
2018-02-09 21:57:14 +0530

    experiment and homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ExperimentComponent } from './components/experiment/experiment.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+const appRoutes: Routes =[
+  {path:'',component: HomePageComponent,pathMatch:'full'},
+  {path:'test',component:ExperimentComponent, pathMatch:'full'}
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperimentComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
