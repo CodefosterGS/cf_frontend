cd08ed6f8b8b1d724edf5172554af07f6384f0eb
himsahu97 <himsahu97@gmail.com>
2018-02-10 00:08:55 +0530

    homepage layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 
+import { SlickModule } from 'ngx-slick';
+
 import { AppComponent } from './app.component';
 import { ExperimentComponent } from './components/experiment/experiment.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 const appRoutes: Routes =[
   {path:'',component: HomePageComponent,pathMatch:'full'},
@@ -16,11 +20,14 @@ const appRoutes: Routes =[
   declarations: [
     AppComponent,
     ExperimentComponent,
-    HomePageComponent
+    HomePageComponent,
+    NavBarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    SlickModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
