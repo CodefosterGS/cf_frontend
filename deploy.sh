1c1b09e5e97d7eced513de40b7a2a00805868421
Ashish Kumar Patel <ashish_3805@hotmail.com>
2018-02-19 23:39:47 +0530

    deploy cp

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 ssh -tt ubuntu@codefoster.club <<EOF
   cd ~
-  git clone https://github.com/CodefosterGS/cf_frontend.git build
-  cd build
+  git clone https://github.com/CodefosterGS/cf_frontend.git
+  cd ./cf_frontend
   npm install
   ng build --prod --build-optimizer
   rm -rf /var/www/codefoster/public
-  cd dist
+  cd ./dist
   cp -r . /var/www/codefoster/public
   exit
 EOF
